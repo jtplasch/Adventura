e74b5f5cb733e7c60da5b30031c0df252f785c90
Justin plaschka <jtplasch@gmail.com>
2021-03-08 12:00:08 -0500

    first commit

---
+++ Adventura/Adventura.WebAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Adventura.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
