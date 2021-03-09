763dc721e8315d77552a797ad948aade707e6cee
Justin plaschka <jtplasch@gmail.com>
2021-03-09 11:00:30 -0500

    first commit of Adventure classes/controllers

--- Adventura/Adventura.Models/AdventureCreate.cs
+++ Adventura/Adventura.Models/AdventureCreate.cs
@@ -5,26 +5,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Adventura.Data
+namespace Adventura.Models
 {
-    public class Adventure
+    public class AdventureCreate
     {
-        [Key]
-        public int AdventureId { get; set; }
-
-        [Required]
-        public Guid OwnerId { get; set; }
-
         [Required]
         [MaxLength(750, ErrorMessage = "You have reached the character limit.")]
         public string Title { get; set; }
 
         [Required]
-        [Display(Name = "Enter the City/State this adventure took place:")]
-        public string Location { get; set; }
         [MaxLength(750, ErrorMessage = "You have reached the character limit.")]
-        [Display(Name = "Enter activities and places of interest you visited:")]
+        public string Location { get; set; }
+
+        [MaxLength(1200, ErrorMessage = "You have reached the character limit.")]
         public string Activities { get; set; }
-        public DateTimeOffset CreatedUtc { get; set; }
+
     }
 }
