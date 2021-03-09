f7998d2387cc4743938a4a588eb1af171a50fb02
Your Name <you@example.com>
2021-03-09 16:31:27 -0500

    initial

---
+++ Adventura/Adventura.Models/ActivityListItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventura.Models
+{
+    public class ActivityListItem
+    {
+        public int ActivityId { get; set; }
+
+        public TypeOfActivity ActivityType { get; set; }
+
+        [MaxLength(100, ErrorMessage = "There are too many characters in this field.")]
+        public string ActivityDescription { get; set; }
+    }
+}
