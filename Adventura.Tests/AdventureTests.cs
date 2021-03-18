b75bc79a201ea50c3b687ec246e96f800524f613
DevonRemaley <Devonremaley@gmail.com>
2021-03-18 10:51:40 -0500

    startedTesting

--- Adventura.Tests/AdventureTests.cs
+++ Adventura.Tests/AdventureTests.cs
@@ -1,14 +1,36 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Adventura.Data;
+using Adventura.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 
 namespace Adventura.Tests
 {
+    
     [TestClass]
     public class AdventureTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        public static Guid userId = new Guid("300faab2-c025-4b37-a115-fbd82b6fdb9e");
+        private AdventureService _repo = new AdventureService(userId);
+        private List<Adventure> _adventures = new List<Adventure>();
+        [TestInitialize]
+        public void Seed()
         {
+            _adventures = new List<Adventure>();
+            _repo = new AdventureService(userId);
+            Adventure seedAdventure = new Adventure();
+            {
+                seedAdventure.AdventureId = 3004;
+                seedAdventure.Title = "Beaches of Jamaica";
+                seedAdventure.Description = null;
+            };
+            _adventures.Add(seedAdventure);
         }
     }
+    /*
+    [TestMethod]
+    public void CreateAdventureTest()
+    {
+    }
+    */
 }
