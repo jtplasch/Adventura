e67770cd4ee8cc455a33cbe8c7645bbb531c96d6
bryce-lm <brycetothelee@gmail.com>
2021-03-18 11:26:35 -0400

    incomplete code

---
+++ Adventura.Tests/LocationTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Adventura.Data;
+
+namespace Adventura.Tests
+{
+    [TestClass]
+    public class LocationUnitTest
+    {
+        [TestMethod]
+        public void locationMethod()
+        {
+            try
+            {
+                Location location = new Location(5, Jamaica, 3004);
+                Console.WriteLine(location());
+            }
+            catch (ArgumentOutOfRangeException outOfRange)
+            {
+                Console.WriteLine(outOfRange.Message);
+            }
+        }
+    }
+
+    public class LocationUnitTest1
+    {
+        private int LocationId;
+        private string LocationName;
+        private int AdventureId;
+
+        [TestMethod]
+        public LocationUnitTest1(int locId, string locName, int advId)
+        {
+            LocationId = locId;
+            LocationName = locName;
+            AdventureId = advId;
+        }
+    }
+}
+
